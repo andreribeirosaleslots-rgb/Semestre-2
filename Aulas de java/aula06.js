373ce1f11fb45e112ff136f439ee7a0c863f37d1
andre <andre.ribeirosaleslots@gmail.com>
2026-02-13 09:48:38 -0300

    Aulas de JavaScript0.2

---
+++ Aulas de java/aula06.js
@@ -0,0 +1,9 @@
+var num1 = 5;
+var num2 = -1;
+
+if (num1 > 0 && num2 > 0) {
+    console.log("A primeira variavel e maior que a segunda e a segunda varivel e um numero positivo");}
+if (num1 > num2 && num2 <= 0) {
+    console.log("A primeira variavel e maior que a segunda e a segunda varivel e um numero negativo ou zero");}
+if (num1 < num2 || num2 > 0) {
+    console.log("A primeira variavel e menor que a segunda ou a segunda varivel e um numero positivo");}
